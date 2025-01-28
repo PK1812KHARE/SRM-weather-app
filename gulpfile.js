9eabe8282c4cf307ccefe0af5de2bbf411f7a5b5
PK1812KHARE <kharepathik@gmail.com>
2025-01-28 18:30:20 +0530

    Initial commit - My custom weather app

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sync = require("browser-sync").create();
+
+function generateCSS(cb) {
+  src("./sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./assets/compiled-css"))
+    .pipe(sync.stream());
+  cb();
+}
+exports.css = generateCSS;
+
+function watchFiles(cb) {
+  watch("./sass/*.scss", generateCSS);
+}
+exports.watch = watchFiles;
+
+function browserSync(cb) {
+  sync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  watch("./sass/*.scss", generateCSS);
+  watch("./*.html").on("change", sync.reload);
+}
+exports.sync = browserSync;
+
+// ✅ Add a Default Task Here
+exports.default = series(generateCSS, browserSync);
